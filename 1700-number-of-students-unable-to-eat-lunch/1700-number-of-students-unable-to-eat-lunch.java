78482a37221a56c1674f25d8371e60d50b60d218
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-04-08 18:01:08 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (53.97%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,26 +1,23 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Stack<Integer> studentStack = new Stack<>();
-        int currentStudentIndex = 0;
-        for(int i =0;i< sandwiches.length;i++){
-            if(!studentStack.isEmpty() && studentStack.peek() == sandwiches[i]){
-                studentStack.pop();
-            } else if(currentStudentIndex == students.length){
-               return studentStack.size();
-               // break;
+        // count the number of students who wants sandwiches of each types
+        int zeroes = 0;
+        int ones = 0;
+        for(int i : students){
+            if(i == 0)zeroes++;
+            else ones++;
+        }
+        // now according to the sandwich type at the top of the stack update the counter
+        for(int i : sandwiches){
+            if(i == 0){
+                // if there are no sandwiches left of type 0 return number of student left for type 1
+                if(zeroes ==0)return ones;
+                zeroes--;
+            } else{
+                if(ones == 0)return zeroes;
+                ones--;
             }
-            else {
-                for(int j = currentStudentIndex;j<students.length;j++){
-                    if(sandwiches[i] != students[j]) studentStack.push(students[j]);
-                    else{
-                        studentStack.push(students[j]);
-                        currentStudentIndex = ++j;
-                        break;
-                    }
-                    if(j == students.length-1)currentStudentIndex = ++j;
-                }
-                i--;
-            }
+            
         }
         return 0;
     }
