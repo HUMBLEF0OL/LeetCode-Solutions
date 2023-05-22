350aac21d1eb10ac2f8e5a7eb2fad17e4c742a4e
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-22 07:57:15 +0530

    Time: 67 ms (25.35%), Space: 41.8 MB (95.85%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,10 +5,10 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let result = init;
-    for(let i =0;i<nums.length;i++){
-        result = fn(result,nums[i]);
-    }
-    return result;
+    let accumulator = init;
+  nums.forEach((element) => {
+    accumulator = fn(accumulator, element);
+  });
+  return accumulator;
     
 };
