ad2444dba9b85c83448d0ff12f882a8f15a356c1
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-05-24 08:25:08 +0530

    Time: 63 ms (44.62%), Space: 42 MB (40.47%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -5,14 +5,16 @@
  */
 var timeLimit = function(fn, t) {
 	return async function(...args) {
-        return new Promise((resolve,reject)=>{
+        let fns = fn(...args);
+        let prms = new Promise((resolve,reject)=>{
             setTimeout(()=>{
-               reject("Time Limit Exceeded"); 
+                reject("Time Limit Exceeded");
             },t);
-            fn(...args).then(resolve).catch(reject);
         })
+        return Promise.race([fns,prms]);
     }
 };
+
 
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
