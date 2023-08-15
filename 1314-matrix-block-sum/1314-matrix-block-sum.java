e3a043fce7990cd545521d7af75b66e7df1eaf1d
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2023-08-15 16:20:29 +0530

    Time: 6 ms (55.65%), Space: 44.4 MB (91.37%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int findSum(int rl,int rh,int cl,int ch,int mat[][])
-    {
-        int sum = 0;
-        for(int i =rl;i<=rh;i++)
-        {
-            for(int j = cl;j<= ch;j++)
-                sum+=mat[i][j];
-        }
-        return sum;
-    }
     public int[][] matrixBlockSum(int[][] mat, int k) {
-        int output[][] = new int[mat.length][mat[0].length];
-        for(int i =0;i<mat.length;i++)
-        {
-            for(int j =0;j<mat[0].length;j++)
-            {
-                int rL = Math.max(0,i-k);
-                int rH = Math.min(mat.length-1,i+k);
-                
-                int cL = Math.max(0,j-k);
-                int cH = Math.min(mat[0].length-1,j+k);
-                output[i][j] = findSum(rL,rH,cL,cH,mat);
-            }
+    int m = mat.length;
+    int n = mat[0].length;
+    int[][] answer = new int[m][n];
+
+    // Compute the prefix sum matrix
+    int[][] prefixSum = new int[m + 1][n + 1];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            prefixSum[i + 1][j + 1] = mat[i][j] + prefixSum[i][j + 1] + prefixSum[i + 1][j] - prefixSum[i][j];
         }
-        return output;
     }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            int r1 = Math.max(0, i - k);
+            int c1 = Math.max(0, j - k);
+            int r2 = Math.min(m - 1, i + k);
+            int c2 = Math.min(n - 1, j + k);
+
+            answer[i][j] = prefixSum[r2 + 1][c2 + 1] - prefixSum[r2 + 1][c1] - prefixSum[r1][c2 + 1] + prefixSum[r1][c1];
+        }
+    }
+
+    return answer;
+}
+
 }
