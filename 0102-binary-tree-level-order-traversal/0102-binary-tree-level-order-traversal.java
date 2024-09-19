381b5749314a0df325bb440961dce0511ca8796e
AMIT RANA <69069897+HUMBLEF0OL@users.noreply.github.com>
2024-09-19 07:31:22 +0530

    Time: 2 ms (14.95%), Space: 45 MB (39.48%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,33 +15,31 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(root == null)return result;
-        List<Integer> levelElements = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        Queue<TreeNode> levels = new LinkedList<>();
+        levels.add(root);
+        /* add delimiter */
+        levels.add(null);
+        List<Integer> nodes = new ArrayList<>();
         
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        queue.add(null);
-        while(!queue.isEmpty()){
-            TreeNode head = queue.remove();
-            levelElements.add(head.val);
-            if(head.left != null){
-                queue.add(head.left);
-            }
-            if(head.right != null){
-                queue.add(head.right);
+        while(!levels.isEmpty()){
+            TreeNode currentLevel = levels.remove();
+            nodes.add(currentLevel.val);
+            
+            if(currentLevel.left != null) levels.add(currentLevel.left);
+            if(currentLevel.right != null) levels.add(currentLevel.right);
+            
+            if(levels.peek() == null){
+                list.add(new ArrayList<>(nodes));
+                nodes = new ArrayList<>();
+                levels.add(levels.remove());
+                if(levels.size() == 1)break;
             }
             
-            if(queue.peek() == null) {
-                result.add(levelElements);
-                levelElements = new ArrayList<>();
-                if(queue.size() == 1)
-                    break;
-                else
-                    queue.add(queue.remove());
-            }
         }
-        return result;
-        
+        return list;
     }
 }
